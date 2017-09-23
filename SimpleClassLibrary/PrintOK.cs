28b9cbec82d150b8ef2785d087417c178165449b
pkirklewski <kirklewskis@gmail.com>
2017-09-23 13:59:14 +0100

    Bacukp - All Working

---
+++ SimpleClassLibrary/PrintOK.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace SimpleClassLibrary
+{
+    public class PrintOK
+    {
+        public void WriteOK(){
+         Console.WriteLine("OK");
+         Console.ReadLine();
+        }  
+    }
+}
---
+++ UsingClassLibrary/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SimpleClassLibrary;
+
+namespace UsingClassLibrary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintOK p = new PrintOK();
+            p.WriteOK();
+        }
+    }
+}
